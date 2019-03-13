ec4ee3f3b65e450262ed7c06165d6d593da4c588
gtarcea <glenn.tarcea@gmail.com>
2019-03-13 16:43:59 -0400

    Add method to track process attributes on a per sample basis

--- internal/spreadsheet/model/sample.go
+++ internal/spreadsheet/model/sample.go
@@ -1,14 +1,19 @@
 package model
 
 type Sample struct {
-	Name       string
-	Parent     string
-	Row        int
-	Attributes []*Attribute
+	Name         string
+	Parent       string
+	Row          int
+	Attributes   []*Attribute
+	ProcessAttrs []*Attribute
 }
 
 func (s *Sample) AddAttribute(attribute *Attribute) {
 	s.Attributes = append(s.Attributes, attribute)
+}
+
+func (s *Sample) AddProcessAttribute(attribute *Attribute) {
+	s.ProcessAttrs = append(s.ProcessAttrs, attribute)
 }
 
 func NewSample(name string, row int) *Sample {
