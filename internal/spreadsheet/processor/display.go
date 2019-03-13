a71b8eee347949dd09bd142faad58379c597830a
gtarcea <glenn.tarcea@gmail.com>
2019-03-13 16:42:10 -0400

    Refactor formatting to make it easier to read

--- internal/spreadsheet/processor/display.go
+++ internal/spreadsheet/processor/display.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/materials-commons/mcetl/internal/spreadsheet/model"
 )
@@ -15,19 +16,24 @@ func NewDisplayer() *Displayer {
 func (d *Displayer) Apply(processes []*model.Process) error {
 	for _, process := range processes {
 		fmt.Println("Process", process.Name)
-		fmt.Println("   Process Attributes:")
+		fmt.Printf("%sProcess Attributes:\n", spaces(4))
 		for _, pattr := range process.Attributes {
-			fmt.Println("     ", pattr.Name)
+			fmt.Printf("%s%s\n", spaces(6), pattr.Name)
 		}
-		fmt.Println("    Samples:")
+		fmt.Printf("%sSamples:\n", spaces(4))
 		for _, sample := range process.Samples {
-			fmt.Println("        ", sample.Name)
-			fmt.Println("             Attributes:")
+			fmt.Printf("%s%s\n", spaces(6), sample.Name)
+			fmt.Printf("%sAttributes:\n", spaces(8))
 			for _, sattr := range sample.Attributes {
-				fmt.Println("               ", sattr.Name, "/", sattr.Value)
+				fmt.Printf("%s%s: %s(%s)\n", spaces(10), sattr.Name, sattr.Value, sattr.Unit)
 			}
 		}
+		fmt.Println("")
 	}
 
 	return nil
+}
+
+func spaces(count int) string {
+	return strings.Repeat(" ", count)
 }
