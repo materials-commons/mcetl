d6b2f3ee9bc69aefcdff580291765c92a71c684c
gtarcea <glenn.tarcea@gmail.com>
2019-04-12 12:29:29 -0400

    Move ColumnAttribute to its own file
    * Change to ColumnAttributeType
    * Add String method to convert "enum" to a string

---
+++ internal/spreadsheet/column_attr_type.go
@@ -0,0 +1,22 @@
+package spreadsheet
+
+type ColumnAttributeType int
+
+const (
+	SampleAttributeColumn = iota + 1
+	ProcessAttributeColumn
+	FileAttributeColumn
+)
+
+func (c ColumnAttributeType) String() string {
+	switch c {
+	case SampleAttributeColumn:
+		return "SampleAttributeColumn"
+	case ProcessAttributeColumn:
+		return "ProcessAttributeColumn"
+	case FileAttributeColumn:
+		return "FileAttributeColumn"
+	default:
+		return "UnknownAttributeColumn"
+	}
+}
