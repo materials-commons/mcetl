5d6d10c83710fe2fa65b9f7622a8b949058c6afa
gtarcea <glenn.tarcea@gmail.com>
2019-04-12 20:16:31 -0400

    Refactor load main as it was getting very large

--- cmd/mcetl/cmd/load.go
+++ cmd/mcetl/cmd/load.go
@@ -5,9 +5,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/materials-commons/mcetl/internal/spreadsheet/model"
-
 	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
+
+	"github.com/materials-commons/mcetl/internal/spreadsheet/model"
 
 	"github.com/materials-commons/config"
 	mcapi "github.com/materials-commons/gomcapi"
@@ -35,71 +36,54 @@ func init() {
 }
 
 func cliCmdLoad(cmd *cobra.Command, args []string) {
-	var (
-		baseDir   string
-		file      string
-		projectId string
-		name      string
-		err       error
-	)
-	if file, err = cmd.Flags().GetString("file"); err != nil {
-		fmt.Println("error", err)
-		os.Exit(1)
-	}
-
-	if projectId, err = cmd.Flags().GetString("project-id"); err != nil {
-		fmt.Println("error", err)
-		os.Exit(1)
-	}
-
-	if name, err = cmd.Flags().GetString("experiment-name"); err != nil {
-		fmt.Println("error", err)
-		os.Exit(1)
-	}
-
-	if baseDir, err = cmd.Flags().GetString("project-base-dir"); err != nil {
-		fmt.Println("error", err)
-		os.Exit(1)
-	}
-
-	mcurl := config.GetString("mcurl")
-	if mcurl == "" {
-		if mcurl, err = cmd.Flags().GetString("mcurl"); err != nil {
-			fmt.Println("error", err)
-			os.Exit(1)
-		}
-	}
-	fmt.Println("Using mcurl:", mcurl)
-
-	apikey := config.GetString("apikey")
-	if apikey == "" {
-		if apikey, err = cmd.Flags().GetString("apikey"); err != nil {
-			fmt.Println("error", err)
-			os.Exit(1)
-		}
-	}
-	fmt.Println("Using apikey:", apikey)
-
-	worksheets, err := spreadsheet.Load(file)
+	worksheets, err := loadSpreadsheet(cmd)
 	if err != nil {
-		fmt.Println("Loading spreadsheet failed:")
-		if merr, ok := err.(*multierror.Error); ok {
-			for _, e := range merr.Errors {
-				fmt.Println(" ", e)
-			}
-		}
 		os.Exit(1)
 	}
 
 	// add baseDir to all file entries in the worksheets
-	addBaseDirToFilePaths(baseDir, worksheets)
-
-	client := mcapi.NewClient(mcurl)
-	client.APIKey = apikey
-
-	if err := spreadsheet.Create(projectId, name, client).Apply(worksheets); err != nil {
-		fmt.Println("Unable to process spreadsheet:", err)
+	if err := addBaseDirToFilePaths(cmd, worksheets); err != nil {
 		os.Exit(1)
+	}
+
+	client, err := createAPIClient(cmd)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if err := createWorkflowFromWorksheets(cmd, client, worksheets); err != nil {
+		os.Exit(1)
+	}
+}
+
+// loadSpreadsheet loads the excel spreadsheet file given in the file flag and
+// transforms it into the internal representation of worksheets.
+func loadSpreadsheet(cmd *cobra.Command) ([]*model.Worksheet, error) {
+	var (
+		file string
+		err  error
+	)
+
+	if file, err = cmd.Flags().GetString("file"); err != nil {
+		fmt.Println("error", err)
+		return nil, err
+	}
+
+	worksheets, err := spreadsheet.Load(file)
+	if err != nil {
+		printLoadSpreadsheetErrors(err)
+		return nil, errors.Errorf("failed loading file")
+	}
+
+	return worksheets, nil
+}
+
+func printLoadSpreadsheetErrors(err error) {
+	fmt.Println("Loading spreadsheet failed:")
+	if merr, ok := err.(*multierror.Error); ok {
+		for _, e := range merr.Errors {
+			fmt.Println(" ", e)
+		}
 	}
 }
 
@@ -108,12 +92,74 @@ func cliCmdLoad(cmd *cobra.Command, args []string) {
 // baseDir to those entries. File entries in a spreadsheet are relative to the
 // location of the spreadsheet. The baseDir represents this path within the
 // context of the project on the server.
-func addBaseDirToFilePaths(baseDir string, worksheets []*model.Worksheet) {
-	for _, worksheet := range worksheets {
-		for _, sample := range worksheet.Samples {
-			for _, file := range sample.Files {
-				file.Path = filepath.Join(baseDir, file.Path)
+func addBaseDirToFilePaths(cmd *cobra.Command, worksheets []*model.Worksheet) error {
+	if baseDir, err := cmd.Flags().GetString("project-base-dir"); err != nil {
+		fmt.Println("error", err)
+		return err
+	} else {
+		for _, worksheet := range worksheets {
+			for _, sample := range worksheet.Samples {
+				for _, file := range sample.Files {
+					file.Path = filepath.Join(baseDir, file.Path)
+				}
 			}
 		}
 	}
+
+	return nil
+}
+
+// createAPIClient creates a mcapi.Client setting the url and apikey
+// from the mcurl and apikey environment variables or command line parameters.
+func createAPIClient(cmd *cobra.Command) (*mcapi.Client, error) {
+	var err error
+	mcurl := config.GetString("mcurl")
+	if mcurl == "" {
+		if mcurl, err = cmd.Flags().GetString("mcurl"); err != nil {
+			fmt.Println("error", err)
+			return nil, err
+		}
+	}
+	fmt.Println("Using mcurl:", mcurl)
+
+	apikey := config.GetString("apikey")
+	if apikey == "" {
+		if apikey, err = cmd.Flags().GetString("apikey"); err != nil {
+			fmt.Println("error", err)
+			return nil, err
+		}
+	}
+	fmt.Println("Using apikey:", apikey)
+
+	client := mcapi.NewClient(mcurl)
+	client.APIKey = apikey
+
+	return client, nil
+}
+
+// createWorkflowFromWorkWorksheets creates the server side workflow from the worksheets.
+func createWorkflowFromWorksheets(cmd *cobra.Command, client *mcapi.Client, worksheets []*model.Worksheet) error {
+	var (
+		projectId      string
+		experimentName string
+		err            error
+	)
+
+	if projectId, err = cmd.Flags().GetString("project-id"); err != nil {
+		fmt.Println("error", err)
+		return err
+	}
+
+	if experimentName, err = cmd.Flags().GetString("experiment-name"); err != nil {
+		fmt.Println("error", err)
+		return err
+	}
+
+	// Create the server side representation of the workflow from the spreadsheet
+	if err := spreadsheet.Create(projectId, experimentName, client).Apply(worksheets); err != nil {
+		fmt.Println("Unable to process spreadsheet:", err)
+		return err
+	}
+
+	return nil
 }
